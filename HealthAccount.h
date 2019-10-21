0d1b5a8463569e363fa6f2fbd9017237412b735e
vanleuca2016 <49379240+vanleuca2016@users.noreply.github.com>
2019-10-21 01:10:19 -0700

    Add files via upload

---
+++ HealthAccount.h
@@ -0,0 +1,13 @@
+
+#include <iostream>
+#include <string>
+using namespace std;
+class Healthcare {
+private:
+	string first_name;
+	string last_name;
+public:
+	first_name = fname;
+	last_name = lname;
+};
+
